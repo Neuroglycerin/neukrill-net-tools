4eddba9436fb5b4a147a91f04524118eab3178f3
Finlay Maguire <finlaymaguire@gmail.com>
2014-12-29 20:42:49 +0000

    MVP: train now dumps model for parsing by test.py

--- train.py
+++ train.py
@@ -7,8 +7,15 @@ import numpy as np
 import glob
 import os
 import neukrill_net.utils as utils
+import sklearn.preprocessing
+import sklearn.ensemble
+import sklearn.linear_model
+import sklearn.cross_validation
+from sklearn.externals import joblib
+import sklearn.metrics
 
-def parse_data():
+
+def parse_train_data():
     """
     Parse training data and rescale
     """
@@ -35,10 +42,13 @@ def parse_data():
 
         for index in range(num_image):
             image = skimage.io.imread(image_fpaths[index])
+            #image_ratio = get_minor_major_ratio(image)
+
             resized_image = skimage.transform.resize(image, (25,25))
             image_vector = resized_image.ravel()
             image_array[index,] = image_vector
             array_labels = num_image * [class_name]
+
         class_index += 1
         train_data.append(image_array)
         train_labels = train_labels + array_labels
@@ -48,11 +58,70 @@ def parse_data():
 
     return X_train, y_train
 
+def get_largest_image_region(props, labels, thresholded_image):
+    """
+    iterate over the image regions to find the largest one
+    """
+    region_max_prop = None
+    for regionprop in props:
+        if sum(thresholded_image[labels == regionprop.label])*1.0/regionprop.area < 0.50:
+            continue
+        if regionmaxprop is Nonr:
+            regionmaxprop = regionprop
+        if regionmaxprop.filled_area < regionprop.filled_area:
+            regionmaxprop = regionprop
+    return regionmaxprop
+
+
+def get_minor_major_ratio(image):
+    """
+    calculate the minor and major ratios for an image
+    """
+    image = image.copy()
+    # Create the thresholded image to eliminate some of the background
+    imagethr = np.where(image > np.mean(image),0.,1.0)
+
+    #Dilate the image
+    imdilated = skimage.morphology.dilation(imagethr, np.ones((4,4)))
+
+    # Create the label list
+    label_list = measure.label(imdilated)
+    label_list = imagethr*label_list
+    label_list = label_list.astype(int)
+
+    region_list = skimage.measure.regionprops(label_list)
+    maxregion = get_largest_image_region(region_list, label_list, imagethr)
+
+    # guard against cases where the segmentation fails by providing zeros
+    ratio = 0.0
+    if ((not maxregion is None) and  (maxregion.major_axis_length != 0.0)):
+        ratio = 0.0 if maxregion is None else  maxregion.minor_axis_length*1.0 / maxregion.major_axis_length
+    return ratio
+
 
 def main():
-    X, y = parse_data()
+    X, y = parse_train_data()
 
+    label_encoder = sklearn.preprocessing.LabelEncoder()
+    y = label_encoder.fit_transform(y)
 
+    #clf = sklearn.linear_model.SGDClassifier(n_jobs=-1,
+    #                                         loss='log')
+
+    clf = sklearn.ensemble.RandomForestClassifier(n_jobs=-1,
+                                                  n_estimators=100)
+
+    cv = sklearn.cross_validation.StratifiedShuffleSplit(y)
+
+    results = []
+    for train, test in cv:
+        clf.fit(X[train], y[train])
+        p = clf.predict_proba(X[test])
+        results.append(sklearn.metrics.log_loss(y[test], p))
+
+    print(results)
+
+    joblib.dump(clf, 'model.pkl')
 
 if __name__=='__main__':
     main()
