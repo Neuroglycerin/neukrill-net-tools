36435dea1409fbe10bd10c43fc23d59bb15fbe4d
Finlay Maguire <finlaymaguire@gmail.com>
2014-12-29 15:55:19 +0000

    Added tox support

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='neukrill-net',
+      version='1.0',
+      description='Neukrill-net NDSB tools',
+      author='neuroglycerin',
+      author_email='root@finlaymagui.re',
+      packages=['neukrill_net'])
