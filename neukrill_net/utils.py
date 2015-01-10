e5f8014eafbae1ae9ad17d7958d3a455f1aaa8e6
Gavin <gavingray1729@gmail.com>
2015-01-10 19:01:52 +0000

    new utils for loading data and processing at load time

--- neukrill_net/utils.py
+++ neukrill_net/utils.py
@@ -61,3 +61,49 @@ def check_data_dir(settings):
 
     return settings
 
+def load_images(image_fname_dict, processing=None, verbose=False):
+    """Loads images and applies a processing
+    function if supplied one.
+    
+    Processing function is expected to take a 
+    single argument, the image as a numpy array,
+    and process it.
+    
+    Returns two lists:
+        * data - list of image vectors
+        * labels - list of labels"""
+    if not processing and verbose:
+        print("Warning: no processing applied, it will 
+        not be possible to stack these images due to
+        varying sizes.")
+
+    # initialise lists
+    data = []
+    labels = []
+    class_label_list = []
+    for class_index, class_name in enumerate(image_fname_dict.keys()):
+        if verbose:
+            print("class: {0} of 120: {1}".format(class_index, class_name))
+        image_fpaths = image_fname_dict[class_name]
+        num_image = len(image_fpaths)
+        #image_array = np.zeros((num_image, 625))
+
+        class_label_list.append(class_name)
+        for index in range(num_image):
+            # read the image into a numpy array
+            image = skimage.io.imread(image_fpaths[index])
+            
+            if processing:
+                resized_image = processing(image)
+                image_vector = resized_image.ravel()
+            else:
+                image_vector = image.ravel()
+
+            #image_array[index,] = image_vector
+            data.append(image_vector)
+
+        # generate the class labels and add them to the list
+        array_labels = num_image * [class_name]
+        labels = labels + array_labels
+
+    return data, labels
