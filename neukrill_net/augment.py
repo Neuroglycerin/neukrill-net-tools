c753b3fcd45c316a6b89b0a260214fc77d2dce2d
Gavin <gavingray1729@gmail.com>
2015-01-18 16:29:13 +0000

    added augment.py file with simple wrapper and rotations function

---
+++ neukrill_net/augment.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+"""
+Data Augmentation wrappers and functions.
+Functions to be used as "processing" arguments
+are here.
+"""
+
+import neukrill_net.image_processing as image_processing
+
+def augmentation_wrapper(augment_settings):
+    """
+    Takes settings for augmentation as a dictionary
+    and produces a "processing" function to  
+    """
+    # components of the processing pipeline
+    components = []
+    # at the moment can only do resize and rotate
+    if "resize" in augment_settings:
+        # apply our resize
+        resize = lambda image: image_processing.resize_image(image,
+                                        augment_settings['resize']))
+    else:
+        resize = lambda image: image
+
+    if "rotate" in augment_settings:
+        # apply rotate with options
+        # will require a function here to call it a number of times
+        rotate = lambda image: rotations(image, augment_settings)
+    else:
+        rotate = lambda image: image
+
+    if "flip" in augment_settings:
+        flip = lambda image: image_processing.flip_image(image, 
+                                            augment_settings['flip'])
+        processing = lambda image: rotate(resize(image)) + [flip(resize(image))]
+    else:
+        processing = lambda image: rotate(resize(image))
+
+    return processing
+
+def rotations(image, augment_settings):
+    """
+    Returns a number of rotations depending on the settings.
+    """
+    rotations = lambda image: [image_processing.rotate_image(angle) 
+        for angle in np.linspace(0,2*np.pi,augment_settings['rotate'])]
+    return rotations
+
--- neukrill_net/image_processing.py
+++ neukrill_net/image_processing.py
@@ -5,7 +5,7 @@ Module for all image processing tools
 
 import skimage.transform
 import skimage.io
-
+import skimage.util
 
 def load_images(image_fpaths, processing, verbose=False):
     """
@@ -47,8 +47,39 @@ def resize_image(image, size):
     input: image
            size e.g. the tuple (48,48)
     output: resized_image
+
+    does this by padding to make the image square, then
+    resizing
     """
-    resized_image = skimage.transform.resize(image, size)
+    # find out how much we have to pad
+    diff = abs(image.shape[1]-image.shape[0])
+    # check if difference is divisible by two
+    if diff%2 > 0:
+        # not divisible, add extra pixel to one side
+        extra = 1
+        # then split to add to each side
+        d = int((diff-1)/2)
+    else:
+        extra = 0
+        d = int(diff/2)
+    # which side to pad?
+    if image.shape[1] > image.shape[0]:
+        # more columns than rows, pad the rows
+        padded_image = skimage.util.pad(image, ((d,d+extra),(0,0)),
+                                            'edge')
+    else:
+        # more rows than columns, pad the columns
+        padded_image = skimage.util.pad(image, ((0,0),(d+extra,d)),
+                                            'edge')
+    if padded_image.shape[0] != padded_image.shape[1]:
+        raise ValueError("Padded image is not square"
+                            "Raise an issue about this."
+                            "print rows: {0}, columns:{1}".format(
+                        padded_image.shape[0],padded_image.shape[1]))
+
+    # now resize to specified size
+    resized_image = skimage.transform.resize(padded_image, size)
+
     return resized_image
 
 def flip_image(image, flip_x=False, flip_y=False):
