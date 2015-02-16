ef19c5086cdb039278b11c92882f2d0f14c04408
Gavin <gavingray1729@gmail.com>
2015-02-16 20:40:37 +0000

    appears to be working, haven't tested thoroughly

--- neukrill_net/dense_dataset.py
+++ neukrill_net/dense_dataset.py
@@ -14,6 +14,8 @@ import skimage.io
 import neukrill_net.utils
 import neukrill_net.augment
 import pylearn2.datasets
+import numpy as np
+import sklearn.preprocessing
 
 class DensePNGDataset(pylearn2.datasets.DenseDesignMatrix):
     """
@@ -22,28 +24,53 @@ class DensePNGDataset(pylearn2.datasets.DenseDesignMatrix):
 
     Parameters
     ----------
-    settings : settings.json file used to find images and control
+    settings_path : settings.json file path used to find images and control
         how they are loaded and processed.
+
     """
-    def __init__(self,settings_path):
+    def __init__(self,settings_path,run_settings,train_or_predict="train"):
         # parse the settings file
         self.settings = neukrill_net.utils.Settings(settings_path)
         # get the processing settings
-        processing_settings = self.settings.user_input['processing']
+        self.run_settings = run_settings
+        processing_settings = self.run_settings["preprocessing"]
         # get a processing function from this
         processing = neukrill_net.augment.augmentation_wrapper(
                                                         processing_settings)
+        # count the images
+        self.N_images = sum(1 for class_label in self.settings.classes
+                for image_path in 
+                self.settings.image_fnames[train_or_predict][class_label])
+        # multiply that count by augmentation factor
+        self.N_images = int(self.N_images*
+                self.run_settings["augmentation_factor"])
         # initialise y vector
         y = []
-        # initialise list of arrays (which will contain images
-        X = []
+        # initialise array
+        X = np.zeros((self.N_images,self.run_settings["final_shape"][0],
+            self.run_settings["final_shape"][1],1))
+        image_index = 0
         # load the images in image_fpaths, iterating and keeping track of class
         for class_label in self.settings.classes:
-            for image_path in self.settings.images_fpaths[class_label]:
+            print(class_label)
+            for image_path in self.settings.image_fnames[
+                                                train_or_predict][class_label]:
                 # load the image as numpy array
                 image = skimage.io.imread(image_path)
                 # apply processing function (get back multiple images)
                 images = processing(image)
                 # for each image store a class label
                 y += [class_label]*len(images)
-
+                # then broadcast each of these images into the empty X array
+                for image in images:
+                    X[image_index,:,:,0] = image
+                    image_index += 1
+        # make sure y is an array
+        y = np.array(y)
+        # count the y labels
+        N_y_labels = len(list(set(y)))
+        # need to encode labels numerically
+        self.label_encoder = sklearn.preprocessing.LabelEncoder()
+        y = self.label_encoder.fit_transform(y)
+        # now run inherited initialisation
+        super(self.__class__,self).__init__(topo_view=X,y=y,y_labels=N_y_labels)
