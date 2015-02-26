8df1e8c2e95b931aee3c983b56c4c9a2c6219862
Dragos Stanciu <dragos.n.stanciu@gmail.com>
2015-02-26 01:14:24 +0000

    Can now specify no of desired keypoints. Fitness is based on -response- value.

--- neukrill_net/image_features.py
+++ neukrill_net/image_features.py
@@ -4,9 +4,14 @@ Module for feature detection and description.
 
 Summary of implemented functions:
 
-- FAST keypoints (todo: look into adaptive thresholds to restrict max no of keypoints)
-- ORB keypoints (by default gives maximum 500 keypoints, nothing todo for now)
-- BRISK keypoints (todo: look into thresholds)
+- FAST keypoints
+- ORB keypoints
+- MSER keypoints
+- BRISK keypoints
+
+We can now specify the desired no of keypoints. Fitness is based on 'response' value of keypoints.
+
+TODO: look into adaptive thresholds to generate keypoints if no points are detected with default parameters.
 """
 
 import numpy as np
@@ -14,17 +19,43 @@ import cv2
 from matplotlib import pyplot as plt
 
 
-def get_FAST_keypoints(image):
+
+def sort_keypoints_by_response_and_get_n_best(keypoint_list, n=500):
+    """
+    Sorts keypoint list by "response" field and returns the first n best keypoints. 
+    If the length of the list is smaller than n, than return the whole list.
+    input: keypoint_list - list of keypoints
+           n - no of keypoints to be returned
+    """
+    sortedList = sorted(keypoint_list, key=lambda x: x.response, reverse=True)
+
+    bestNKeypoints = []
+
+    if (len(sortedList) > n):
+        bestNKeypoints = sortedList[:n]
+    else:
+        bestNKeypoints = sortedList
+
+    return bestNKeypoints
+
+
+
+def get_FAST_keypoints(image, n=500):
     """
     Detects keypoints using FAST feature detector. 
     (nonmaxSuppression prevents too many keypoints being detected in the same region. This is enabled by default)
-    input: image
+    input:  image
+            n - max number of returned keypoints (default 500)
     output: list of FAST keypoints
     """
     # Initiate FAST object with default values
     fast = cv2.FastFeatureDetector()
+
     # find keypoints
     keyPoints = fast.detect(image, None)
+
+    # sort by response and return best n keypoints
+    keyPoints = sort_keypoints_by_response_and_get_n_best(keyPoints, n)
 
     return keyPoints
 
@@ -42,35 +73,49 @@ def get_ORB_keypoints(image, n=500):
 
     # Initiate ORB detector
     orb = cv2.ORB(nfeatures = n, edgeThreshold = 0)
+
     # find keypoints
     keyPoints = orb.detect(image, None)
+
+    # sort by response and return best n keypoints
+    keyPoints = sort_keypoints_by_response_and_get_n_best(keyPoints, n)
 
     return keyPoints
 
 
-def get_MSER_keypoints(image):
+def get_MSER_keypoints(image, n=500):
     """
     Detects keypoints using MSER detector. 
-    input image
+    input:  image
+            n - max number of returned keypoints (default 500)
     output: list of MSER keypoints
     """
     # initiate MSER detector
     mser = cv2.MSER()
+
     # find keypoints
     keyPoints = mser.detect(image)
+
+    # sort by response and return best n keypoints
+    keyPoints = sort_keypoints_by_response_and_get_n_best(keyPoints, n)
 
     return keyPoints
 
 
-def get_BRISK_keypoints(image):
+def get_BRISK_keypoints(image, n=500):
     """
     Detects keypoints using BRISK feature detector.
-    input: image
+    input:  image
+            n - max no of returned keypoints (default 500)
     output: list of BRISK keypoints
     """
     # Initiate BRISK detector
     brisk = cv2.BRISK()
+
     # find keypoints
     keyPoints = brisk.detect(image, None)
+
+    # sort by response and return best n keypoints
+    keyPoints = sort_keypoints_by_response_and_get_n_best(keyPoints, n)
 
     return keyPoints
