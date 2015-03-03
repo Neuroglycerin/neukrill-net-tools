79a7b6fe45649b7b8cc6de335ca03022da907da0
Scott Lowe <scottlowe0@gmail.com>
2015-03-03 20:14:21 +0000

    Fix undeclared units

--- neukrill_net/augment.py
+++ neukrill_net/augment.py
@@ -226,12 +226,12 @@ class RandomAugment(object):
         
         # Datatype unit conversion
         # We very probably want this to be float throughout
-        if units == 'float64' or units == 'float':
-            image = skimage.util.img_as_float(image)
-        elif units == 'uint8':
-            image = skimage.util.img_as_ubyte(image)
-        elif units == None or units == 'auto':
+        if 'scale' not in self.settings or self.settings['units'] == None or self.settings['units'] == 'auto':
             pass
+        elif self.settings['units'] == 'float64' or self.settings['units'] == 'float':
+            image = skimage.util.img_as_float(image)
+        elif self.settings['units'] == 'uint8':
+            image = skimage.util.img_as_ubyte(image)
         else:
             raise ValueError('Unrecognised output units: {}'.format(
                                 self.settings['units']))
