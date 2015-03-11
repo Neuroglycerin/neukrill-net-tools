e4c2dc0d39116ffa2bb1587ebccc5ad9c7bf5d3a
Matt Graham <matthew.m.graham@gmail.com>
2015-03-11 22:49:40 +0000

    Various typo and bug fixes to update norm monitor learning rule class.

--- neukrill_net/update_norm_monitor.py
+++ neukrill_net/update_norm_monitor.py
@@ -4,8 +4,11 @@ of gradient based updates.
 """
 
 import six
+import warnings
 from pylearn2.training_algorithms.learning_rule import LearningRule
 from pylearn2.utils import sharedX
+from pylearn2.compat import OrderedDict
+from pylearn2.space import NullSpace
 import theano.tensor as T
 
 class UpdateNormMonitorLearningRule(LearningRule):
@@ -16,9 +19,12 @@ class UpdateNormMonitorLearningRule(LearningRule):
     """
     
     def __init__(self, base_learning_rule, decay=0.9):
-         self.base = base_learning_rule
-         self.decay = decay
-         self.mean_updates = OrderedDict()
+        self.base = base_learning_rule
+        # hack to allow MomentumAdjustor to access momentum value
+        if hasattr(self.base, 'momentum'):
+            self.momentum = self.base.momentum
+        self.decay = decay
+        self.mean_updates = OrderedDict()
          
     def add_channels_to_monitor(self, monitor, monitoring_dataset):
     
@@ -28,33 +34,43 @@ class UpdateNormMonitorLearningRule(LearningRule):
             '_mean': T.mean
         }
         
-        for mean_update in self.grad_norms.values():
+        for mean_update in self.mean_updates.values():
             if mean_update.ndim == 4:
                 # rank-4 tensor (assuming stack of rank-3 convolutional kernels)
                 knl_norm_vals = T.sqrt(T.sum(T.sqr(mean_update), axis=(1,2,3)))
-                for suffix, op in channel_mapping:
+                for suffix, op in channel_mapping.items():
                     monitor.add_channel(
                         name=(mean_update.name + "_kernel_norm" + suffix),
                         ipt=None,
-                        val=op(knl_norm_vals)
+                        val=op(knl_norm_vals),
+                        data_specs=(NullSpace(), ''),
+                        dataset=monitoring_dataset)
+            elif mean_update.ndim == 3:
+                # rank-3 tensor (assuming stack of rank-2 conv layer biases)
+                knl_norm_vals = T.sqrt(T.sum(T.sqr(mean_update), axis=(1,2)))
+                for suffix, op in channel_mapping.items():
+                    monitor.add_channel(
+                        name=(mean_update.name + "_norm" + suffix),
+                        ipt=None,
+                        val=op(knl_norm_vals),
                         data_specs=(NullSpace(), ''),
                         dataset=monitoring_dataset)
             elif mean_update.ndim == 2:
                 # rank-2 tensor (matrix)
                 col_norm_vals = T.sqrt(T.sum(T.sqr(mean_update), axis=0))
                 row_norm_vals = T.sqrt(T.sum(T.sqr(mean_update), axis=1))
-                mtx_norm_val = T.sqrt(T.sum(T.sqr(mean_update))        
-                for suffix, op in channel_mapping:
+                mtx_norm_val = T.sqrt(T.sum(T.sqr(mean_update)))        
+                for suffix, op in channel_mapping.items():
                     monitor.add_channel(
                         name=(mean_update.name + "_col_norm" + suffix),
                         ipt=None,
-                        val=op(col_norm_vals)
+                        val=op(col_norm_vals),
                         data_specs=(NullSpace(), ''),
                         dataset=monitoring_dataset)
                     monitor.add_channel(
                         name=(mean_update.name + "_row_norm" + suffix),
                         ipt=None,
-                        val=op(row_norm_vals)
+                        val=op(row_norm_vals),
                         data_specs=(NullSpace(), ''),
                         dataset=monitoring_dataset)
                 monitor.add_channel(
@@ -83,8 +99,8 @@ class UpdateNormMonitorLearningRule(LearningRule):
             else:
                 # not sure which axes to sum over in this case
                 raise ValueError(
-                    'Mean update {0} has unexpected number of dimensions {0}'
-                    .format(mean_update, mean_update.ndim))
+                    'Mean update {0} has unexpected number of dimensions {1} ({2})'
+                    .format(mean_update, mean_update.ndim, mean_update.shape))
                     
         self.base.add_channels_to_monitor(monitor, monitoring_dataset)
         
@@ -108,7 +124,7 @@ class UpdateNormMonitorLearningRule(LearningRule):
                               "incorrect." % param.name)
                               
             # Store variable in self.mean_updates for monitoring.
-            self.mean_updates[param.name] = mean_updates
+            self.mean_updates[param.name] = mean_update
 
             # Accumulate updates
             d_param = updates[param] - param
